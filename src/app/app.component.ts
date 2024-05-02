437ee65e5b4eb69d4e2a023dfa2f9cfb438b43c2
Andrebond777 <41829674+Andrebond777@users.noreply.github.com>
2024-05-02 02:17:47 +0100

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -101,6 +101,7 @@ export class AppComponent  extends CommonFunctionalityComponent {
 
   async next()
   {
+    this.fetchPhoto()
     this.isShow = false;
     this.roundNumber++;
   }
@@ -145,7 +146,7 @@ export class AppComponent  extends CommonFunctionalityComponent {
 
   constructor(private flickrUiService: FlickrUiService, public override router:Router) {
     super(router);
-    for(let i = 0; i < this.maxRoundNumber; i++)
+    //for(let i = 0; i < this.maxRoundNumber; i++)
       this.fetchPhoto()
   }
 
