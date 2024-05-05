6aead7a7d3396f0f60810430995cfa09dd3a0bcc
Andrebond777 <41829674+Andrebond777@users.noreply.github.com>
2024-05-05 07:26:59 +0100

    s

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
     production: false,
-    apiUrl: "https://photoyearguesserfunction.azurewebsites.net/api"
+    apiUrl: "https://photoguesserf.azurewebsites.net/api"
   };
